b45cdbddd8a3ae14ada30cb3ac07041731f3c80a
ukangaekom <ekomabasiuk@gmail.com>
2025-07-06 08:26:43 +0100

    Working on event files
    Working on the Event Tools

---
+++ src/tools/events.rs
@@ -0,0 +1,27 @@
+use reqwest;
+use scrapper::{Html, Selector};
+
+
+
+
+
+
+pub fn upcoming_events() -> String{
+
+
+}
+
+
+pub fn event_videos() -> String{
+
+
+}
+
+pub fn workshops() -> String{
+
+
+}
+
+pub fn bootcamps() -> String{
+    
+}
